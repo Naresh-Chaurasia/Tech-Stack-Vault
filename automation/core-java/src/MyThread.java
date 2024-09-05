ca8beb0a54fdd4c9ce6c2afe0bbb8fc181961ea1
Naresh Chaurasia <johndoe@example.com>
2024-09-05 22:27:07 +0530

    Macbook Pro, Checkin Timestamp::Thu Sep  5 22:27:07 IST 2024

---
+++ automation/core-java/src/MyThread.java
@@ -0,0 +1,13 @@
+import java.lang.Thread;
+
+public class MyThread {
+    public static void main(String[] args) {
+
+        Thread t1 = Thread.currentThread();
+        String name = t1.getName();
+
+        System.out.println(name);
+
+        System.out.println("My Thread");
+    }
+}
