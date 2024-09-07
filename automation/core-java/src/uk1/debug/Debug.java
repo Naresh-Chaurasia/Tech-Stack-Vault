93f5f1533172191b9b30868432cc46a5b42fe416
Naresh Chaurasia <johndoe@example.com>
2024-09-07 11:53:49 +0530

    Macbook Pro, Checkin Timestamp::Sat Sep  7 11:53:49 IST 2024

---
+++ automation/core-java/src/uk1/debug/Debug.java
@@ -0,0 +1,18 @@
+package uk1.debug;
+
+public class Debug {
+
+    public static void main(String[] args) {
+        
+
+        int a = 10;
+        int b = 20;
+
+        int c = a + b;
+
+        System.out.println(c);
+
+
+    }
+    
+}
