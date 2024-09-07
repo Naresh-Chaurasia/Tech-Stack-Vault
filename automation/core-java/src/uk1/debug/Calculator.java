ddb47f6a1ecce5bb8b74f06ee73ccf388dfb45e5
Naresh Chaurasia <johndoe@example.com>
2024-09-07 12:14:36 +0530

    Macbook Pro, Checkin Timestamp::Sat Sep  7 12:14:36 IST 2024

---
+++ automation/core-java/src/uk1/debug/Calculator.java
@@ -0,0 +1,7 @@
+package uk1.debug;
+
+public class Calculator {
+    public void add(int a, int b){
+        System.out.println(a+b);
+    }
+}
---
+++ automation/core-java/src/uk1/debug/MainCalculator.java
@@ -0,0 +1,10 @@
+package uk1.debug;
+
+public class MainCalculator {
+
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        c.add(10, 20);
+    }
+    
+}
