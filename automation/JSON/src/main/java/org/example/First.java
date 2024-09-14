993e9a8394d9901c048293d01b79867e0f02411e
Naresh Chaurasia <johndoe@example.com>
2024-09-14 09:45:36 +0530

    Macbook Pro, Checkin Timestamp::Sat Sep 14 09:45:36 IST 2024

---
+++ automation/JSON/src/main/java/org/example/First.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class First {
+
+    public static void main(String[] args) {
+        // Read JSON from a file
+        File jsonFile = new File("/Users/nareshchaurasia/nc/Tech-Stack-Vault/automation/JSON/src/main/resources/file1.json");
+    }
+}
